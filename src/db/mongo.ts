43125210540717e77ff3f5d82af3d56c10631841
G.S.Chandhru <gschandhru10@gmail.com>
2025-12-18 23:51:44 +0530

    feat : added dep, models and more methods

---
+++ src/db/mongo.ts
@@ -0,0 +1,32 @@
+import { MongoClient, Db } from 'mongodb';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const uri = process.env.MONGO_URI!;
+const dbName = process.env.MONGO_DB_NAME!;
+
+let client: MongoClient;
+let db: Db;
+
+export const connectMongo = async (): Promise<Db> => {
+  if (db) {
+    return db; // reuse existing connection
+  }
+  console.log('🔌 Connecting to MongoDB...', db);
+  client = new MongoClient(uri);
+  await client.connect();
+
+  db = client.db(dbName);
+  console.log('✅ MongoDB connected');
+  console.log(db)
+
+  return db;
+};
+
+export const getDb = (): Db => {
+  if (!db) {
+    throw new Error('❌ Database not initialized');
+  }
+  return db;
+};
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import Hapi from '@hapi/hapi';
-import { userRoutes } from './User/userRoutes';
+import dotenv from 'dotenv';
+import { userRoutes } from './user/userRoutes';
+import { connectMongo } from './db/mongo';
+
+dotenv.config();
 
 const init = async (): Promise<void> => {
   const server = Hapi.server({
@@ -8,6 +12,8 @@ const init = async (): Promise<void> => {
   });
 
   server.route(userRoutes);
+
+  await connectMongo();
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
